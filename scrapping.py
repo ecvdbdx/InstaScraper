ca500b81f13f3d748521b0486ae350709cb951ef
ECV <ecv@ECV-BO-B018-IM026.local>
2017-12-15 13:24:53 +0100

    bleh

--- scrapping.py
+++ scrapping.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
 
-url = "https://www.instagram.com/explore/tags/plage/"
+url = "https://www.instagram.com/amelieglutenfit/"
 
 driver = webdriver.PhantomJS()
 driver.get(url)
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-for x in soup.findAll('section'):
+for x in soup.findAll('section main '):
     print(x)
